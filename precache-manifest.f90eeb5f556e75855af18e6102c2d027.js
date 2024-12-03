2ac9c50b4149d3a64282f147f8cb8a08756d0734
k1ngeric <154526765+k1ngeric@users.noreply.github.com>
2024-12-03 17:53:52 +0100

    Update precache-manifest.f90eeb5f556e75855af18e6102c2d027.js

---
+++ precache-manifest.f90eeb5f556e75855af18e6102c2d027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e834be730ca07d611db6c2329f420b5",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "e576944e2c216f486dc8",
+    "url": "/first-react-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7f33a16dda33a72e8f21",
+    "url": "/first-react-app/static/css/main.e23fee82.chunk.css"
+  },
+  {
+    "revision": "e576944e2c216f486dc8",
+    "url": "/first-react-app/static/js/2.eeaf7b38.chunk.js"
+  },
+  {
+    "revision": "7f33a16dda33a72e8f21",
+    "url": "/first-react-app/static/js/main.e99f6933.chunk.js"
+  },
+  {
+    "revision": "cb2d0d1ef7d3e2b845c4",
+    "url": "/first-react-app/static/js/runtime~main.f5c4d8dc.js"
+  }
+]);
